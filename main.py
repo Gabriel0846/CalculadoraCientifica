18b6813a3b96bef047905c83fbccbc7d0e558ff4
Gabriel Lopes <144557326+Gabriel0846@users.noreply.github.com>
2024-11-04 12:32:36 -0300

    botão tan

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from numpy.ma.extras import column_stack
 
 cor1 = "#3b3b3b" # preta
 cor2 = "#feffff" # branca
-cor3 = "#424345" # preta
+cor3 = "#37474F" # preta
 cor4 = "#eceff1" # cinza
 cor5 = "#ffab40" # laranja
 
@@ -31,7 +31,11 @@ frame_corpo.grid(row=2, column=0)
 
 
 # configurando frame tela
-label_tela = Label(janela, width=16, height=2, text='123456789',padx=7, anchor='e', bd=0, justify=RIGHT, font=('Ivy 18'), bg=cor3, fg=cor2)
+label_tela = Label(frame_tela, text='123456789', width=16, height=2,padx=7, anchor='e', bd=0, justify=RIGHT, font=('Ivy 18'), bg=cor3, fg=cor2)
 label_tela.place(x=0, y=0)
+
+# configurando flame cientifico
+b_0 = Button(frame_cientifica,text='tan', width=6, height=1,relief=RAISED, overrelief=RIDGE, font=('Ivy 10 bold'), bg=cor1, fg=cor2)
+b_0.place(x=0, y=0)
 
 janela.mainloop()
